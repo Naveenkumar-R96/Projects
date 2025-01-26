57386dd35d39c4e9033770bc22f0368ae8b40ac5
NAVEEN KUMAR <nk946432@gmail.com>
2025-01-26 14:04:35 +0530

    created a ui

---
+++ chatbox/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatBotStart from './components/ChatBotStart/ChatBotStart'
+import ChatBotApp from './components/ChatApp/ChatBotApp'
+
+const App = () => {
+  return (
+    <div className='container'>
+   {/*    <ChatBotStart/> */}
+      <ChatBotApp/>
+    </div>
+  )
+}
+
+export default App
---
+++ chatbox/src/components/ChatBotStart/ChatBotStart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ChatBox.css'
+const ChatBox = () => {
+  return (
+    <div className='start-page'>
+      <button className='start-page-btn'>Chat AI</button>
+    </div>
+  )
+}
+
+export default ChatBox
